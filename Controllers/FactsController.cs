74da8154a6e48fad1bf088a6ab017c77fbbd8757
HP <HP@DESKTOP-LKG2SK9>
2022-09-24 15:57:54 +0530

    Add project files.

---
+++ Controllers/FactsController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FactsWebApp.Data;
+using FactsWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FactsWebApp.Controllers
+{
+    public class FactsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FactsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Facts
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Fact.ToListAsync());
+        }
+
+        // GET: Facts/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
+        // POST: Facts/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(string SearchPhrase)
+        {
+            return View("Index",await _context.Fact.Where(j=>j.FactQuestion.Contains
+            (SearchPhrase)).ToListAsync());
+        }
+
+        // GET: Facts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Fact == null)
+            {
+                return NotFound();
+            }
+
+            var fact = await _context.Fact
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fact == null)
+            {
+                return NotFound();
+            }
+
+            return View(fact);
+        }
+
+        // GET: Facts/Create with added authorization
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Facts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FactQuestion,FactAnswer")] Fact fact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(fact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fact);
+        }
+
+        // GET: Facts/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Fact == null)
+            {
+                return NotFound();
+            }
+
+            var fact = await _context.Fact.FindAsync(id);
+            if (fact == null)
+            {
+                return NotFound();
+            }
+            return View(fact);
+        }
+
+        // POST: Facts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FactQuestion,FactAnswer")] Fact fact)
+        {
+            if (id != fact.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FactExists(fact.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fact);
+        }
+
+        // GET: Facts/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Fact == null)
+            {
+                return NotFound();
+            }
+
+            var fact = await _context.Fact
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fact == null)
+            {
+                return NotFound();
+            }
+
+            return View(fact);
+        }
+
+        // POST: Facts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Fact == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Fact'  is null.");
+            }
+            var fact = await _context.Fact.FindAsync(id);
+            if (fact != null)
+            {
+                _context.Fact.Remove(fact);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FactExists(int id)
+        {
+          return _context.Fact.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Fact.cs
@@ -0,0 +1,15 @@
+﻿namespace FactsWebApp.Models
+{
+    public class Fact
+    {
+        public int Id { get; set; }
+        public string FactQuestion { get; set; }
+
+        public string FactAnswer { get; set; }
+
+        public Fact()
+        {
+
+        }
+    }
+}
